83f038223a07dfd46faaf43726ca42120e68605b
rofegarporya-web <rofegarporya@gmail.com>
2026-07-07 15:39:54 -0700

    Production settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["myproject-ql71.onrender.com"]
 
 
 # Application definition
